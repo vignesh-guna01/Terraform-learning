065048ea6ea3fd7e7ff61fcf196d27e4079e43d3
vignesh-guna01 <abivignesh1308@gmail.com>
2026-08-01 10:01:38 +0530

    Update variables.tf

--- modules/VPC/variables.tf
+++ modules/VPC/variables.tf
@@ -5,13 +5,11 @@ variable "tags" {
 
 variable "vpc_cidr" {
   type        = string
-  default     = 10.0.0.0/16
   description = "vpc for my EC2"
 }
 
 variable "subnet_cidr" {
   type        = string
-  default     = 10.1.0.0/16
   description = "subnet for my EC2"
 }
 
